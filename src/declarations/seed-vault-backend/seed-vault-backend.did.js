b59d864ed1620e1942b5c7c2c73554a4e0433466
Richard Hery <richardhery@MacBookAir.lan>
2025-12-16 06:13:43 -0800

    Updated license

---
+++ src/declarations/seed-vault-backend/seed-vault-backend.did.js
@@ -0,0 +1,72 @@
+export const idlFactory = ({ IDL }) => {
+  const Result_3 = IDL.Variant({ 'ok' : IDL.Null, 'err' : IDL.Text });
+  const Result_2 = IDL.Variant({
+    'ok' : IDL.Tuple(IDL.Vec(IDL.Nat8), IDL.Vec(IDL.Nat8)),
+    'err' : IDL.Text,
+  });
+  const Result_1 = IDL.Variant({
+    'ok' : IDL.Tuple(IDL.Vec(IDL.Nat8), IDL.Vec(IDL.Nat8), IDL.Vec(IDL.Nat8)),
+    'err' : IDL.Text,
+  });
+  const Result = IDL.Variant({ 'ok' : IDL.Nat, 'err' : IDL.Text });
+  return IDL.Service({
+    'add_seed' : IDL.Func(
+        [IDL.Text, IDL.Vec(IDL.Nat8), IDL.Vec(IDL.Nat8)],
+        [Result_3],
+        [],
+      ),
+    'canister_cycles' : IDL.Func([], [IDL.Nat], ['query']),
+    'convert_collected_icp' : IDL.Func([], [Result_3], []),
+    'delete_seed' : IDL.Func([IDL.Text], [Result_3], []),
+    'encrypted_symmetric_key_for_seed' : IDL.Func(
+        [IDL.Text, IDL.Vec(IDL.Nat8)],
+        [IDL.Vec(IDL.Nat8)],
+        [],
+      ),
+    'estimate_cost' : IDL.Func(
+        [IDL.Text, IDL.Nat],
+        [
+          IDL.Record({
+            'icp_e8s' : IDL.Nat,
+            'fallback_used' : IDL.Bool,
+            'cycles' : IDL.Nat,
+          }),
+        ],
+        [],
+      ),
+    'get_account_details' : IDL.Func(
+        [],
+        [
+          IDL.Record({
+            'balance' : IDL.Nat,
+            'owner' : IDL.Text,
+            'subaccount' : IDL.Vec(IDL.Nat8),
+            'canister' : IDL.Text,
+          }),
+        ],
+        [],
+      ),
+    'get_seed_cipher' : IDL.Func([IDL.Text], [Result_2], []),
+    'get_seed_cipher_and_key' : IDL.Func(
+        [IDL.Text, IDL.Vec(IDL.Nat8)],
+        [Result_1],
+        [],
+      ),
+    'get_seed_names' : IDL.Func([], [IDL.Vec(IDL.Text)], ['query']),
+    'pricing_status' : IDL.Func(
+        [],
+        [
+          IDL.Record({
+            'fallback_used' : IDL.Bool,
+            'last_rate' : IDL.Nat,
+            'last_refresh_nanoseconds' : IDL.Int,
+          }),
+        ],
+        ['query'],
+      ),
+    'public_key' : IDL.Func([], [IDL.Vec(IDL.Nat8)], []),
+    'seed_count' : IDL.Func([], [IDL.Nat], ['query']),
+    'transfer_icp' : IDL.Func([IDL.Text, IDL.Nat], [Result], []),
+  });
+};
+export const init = ({ IDL }) => { return []; };
